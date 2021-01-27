cca7ce99703b46bcb0d8eff97b9a6f68524d6c65
Colin Chauche <colin.chauche@gmail.com>
2021-01-27 11:18:54 -0500

    Add docker container to simple server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const app = require('./server.js');
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Q&A Server listening on port ${port}`);
