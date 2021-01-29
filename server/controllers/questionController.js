89a1d263339faa4f9b3314380e7135c29a920a23
Colin Chauche <colin.chauche@gmail.com>
2021-01-29 11:57:05 -0500

    Add eslint and pre-commit hooks begin server routes

---
+++ server/controllers/questionController.js
@@ -0,0 +1,5 @@
+module.exports = {
+  getProductQuestions: (req, res => {
+    res.status(200).send('Product Questions');
+  }),
+};
---
+++ server/routes/questionRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const questionController = require('../controllers/questionController.js');
+//todo: import controllers
+
+
+router.get('/', /* Add controller */);
+
+
+
+
+module.exports = router;
