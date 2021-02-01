edbc6bd57ad0eaef38a1abb9cd44ec7a19be8e26
Colin Chauche <colin.chauche@gmail.com>
2021-02-01 11:14:33 -0500

    Add express-validator and implement for getQuestions

--- server/controllers/questionController.js
+++ server/controllers/questionController.js
@@ -1,24 +1,26 @@
 const { Question } = require('../db/mongo.js');
+const { validationResult } = require('express-validator');
 
 module.exports = {
   getQuestions: (req, res) => {
-    let productId = req.query.product_id;
-    if (!productId) {
-      res.status(422).send('Error: invalid product_id provided');
-    } else {
-      // todo: Would like to convert these to numbers using middleware
-      productId = parseInt(productId);
-      let page = req.query.page ? parseInt(req.query.page) : undefined;
-      let count = req.query.count ? parseInt(req.query.count) : undefined;
-      Question.findByProductId(productId, page, count)
-        .then(result => {
-          res.status(200).json(result);
-        })
-        .catch(err => {
-          console.error(err);
-          res.sendStatus(500);
-        });
+    // Responds with error if any of the route validations fail
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
     }
+
+    // let productId = req.query.product_id;
+    // let page = req.query.page ? parseInt(req.query.page) : undefined;
+    // let count = req.query.count ? parseInt(req.query.count) : undefined;
+    Question.findByProductId(req.query.product_id, req.query.page, req.query.count)
+    // Question.findByProductId(productId, page, count)
+      .then(result => {
+        res.status(200).json(result);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(422).send(err.message);
+      });
 
   },
   addQuestion: (req, res) => {
--- server/routes/questionRoutes.js
+++ server/routes/questionRoutes.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
 const questionController = require('../controllers/questionController.js');
+const { query } = require('express-validator');
 
-router.get('/', questionController.getQuestions);
+router.get('/', [
+  query('product_id').exists().isNumeric().toInt(),
+  query('page').if(query('page').exists()).isNumeric().toInt(),
+  query('count').if(query('count').exists()).isNumeric().toInt(),
+],
+questionController.getQuestions);
+
+// body should exist
+// name should exist
+// standardize e-mail
+// product_id should exist convert to number
 router.post('/', questionController.addQuestion);
+
+// question_id should exist and convert to string
 router.put('/:question_id/helpful', questionController.markQuestionHelpful);
 router.put('/:question_id/report', questionController.reportQuestion);
 
