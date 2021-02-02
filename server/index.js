15ab9aff7d41b4ae9002ea07043fc0620e806a42
Colin Chauche <colin.chauche@gmail.com>
2021-02-01 19:46:01 -0500

    Restore server port to 3000

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('./server.js');
 
-const SERVER_PORT = process.env.SERVER_PORT || 3010;
+const SERVER_PORT = process.env.SERVER_PORT || 3000;
 
 app.listen(SERVER_PORT, () => {
   console.log(`Q&A Server listening on port ${SERVER_PORT}`);
