5153a3940065bba49f3de4d6cecd4914b0f460e4
Colin Chauche <colin.chauche@gmail.com>
2021-02-01 19:11:01 -0500

    Enable CORS headers and add morgan to server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('./server.js');
 
-const SERVER_PORT = process.env.SERVER_PORT || 3000;
+const SERVER_PORT = process.env.SERVER_PORT || 3010;
 
 app.listen(SERVER_PORT, () => {
   console.log(`Q&A Server listening on port ${SERVER_PORT}`);
--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = require('./routes');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
 
 app.use('/qa/questions', router.questions);
 app.use('/qa/answers', router.answers);
