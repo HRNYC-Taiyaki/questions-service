72aae0d0665e22f6169780f68213b9a099181d8e
Colin Chauche <colin.chauche@gmail.com>
2021-02-04 13:50:13 -0500

    Add test routes to delete added questions/answers

--- server/server.js
+++ server/server.js
@@ -10,9 +10,39 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 
-// For stress testing with loader.io
+// ************************
+// ** For Stress Testing
+// ************************
+const { Answer, Question } = require('./db/mongo.js');
+
+// Delete all questions related to test product 9999999
+app.delete('/qa/questions', (req, res) => {
+  Question.deleteMany({ product_id: 9999999 })
+    .then((result) => {
+      req.status(204).json(result);
+    })
+    .catch((err) => {
+      res.status(500).send(err.message);
+    });
+});
+
+// Delete all answers related to test question '601949a78f1fa4f66235bdf4'
+app.delete('/qa/questions/:question_id/answers', (req, res) => {
+  let question_id = req.params.question_id;
+  Answer.deleteMany({ question_id })
+    .then((result) => {
+      res.status(204).json(result);
+    })
+    .catch((err) => {
+      res.status(500).send(err.message);
+    });
+});
+
+// Returns authentication file for loader.io
 app.get('/loaderio-a4c54f82626e216f9d99d73ad6d702e0/', (req, res) => {
-  res.sendFile(path.join(__dirname, './loaderio-a4c54f82626e216f9d99d73ad6d702e0.txt'));
+  res.sendFile(
+    path.join(__dirname, './loaderio-a4c54f82626e216f9d99d73ad6d702e0.txt')
+  );
 });
 
 app.use('/qa/questions', router.questions);
