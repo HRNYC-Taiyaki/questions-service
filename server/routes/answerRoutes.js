b55ca2a4b6f95336dbb2d2138713fd5b46060831
Colin Chauche <colin.chauche@gmail.com>
2021-02-03 22:35:59 -0500

    Removed comments and un-used imports

--- server/routes/answerRoutes.js
+++ server/routes/answerRoutes.js
@@ -1,26 +1,10 @@
 const router = require('express').Router();
 const answerController = require('../controllers/answerController.js');
-const { query, body, param } = require('express-validator');
+const { param } = require('express-validator');
 
 const validateAnswerId = [
   param('answer_id').exists().isLength({min: 24, max: 24})
 ];
-
-// router.get('/', [
-//   param('question_id').exists().isLength({min: 24, max: 24}),
-//   query('page').optional().isNumeric().toInt(),
-//   query('count').optional().isNumeric().toInt(),
-//   // query('page').if(query('page').exists()).isNumeric().toInt(),
-//   // query('count').if(query('count').exists()).isNumeric().toInt(),
-// ], answerController.getAnswers);
-
-// router.post('/', [
-//   param('question_id').exists().isLength({min: 24, max: 24}),
-//   body('body').isLength({min: 3, max: 1000}).trim().escape(),
-//   body('name').isLength({min: 1, max: 60}).trim().escape(),
-//   body('email').isEmail().normalizeEmail(),
-//   body('photos').optional().isArray( { max: 5 })
-// ], answerController.addAnswer);
 
 router.put('/:answer_id/helpful', validateAnswerId, answerController.markAnswerHelpful);
 
